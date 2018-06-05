16294a259d950a0a8983236f3aa1afab8402bcb2
critsmet <36425627+critsmet@users.noreply.github.com>
2018-06-05 17:07:30 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(person_name)
+  @name = person_name
+
+  def name
+    @name
+  end
+
+  def job=(persons_jobs)
+    @job = (persons_jobs)
+  end
+
+  def job
+    @job
+  end
+end
+end
